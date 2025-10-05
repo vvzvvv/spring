04ddddd5df38f87defc8e5653f51379d6cde8ab1
vvzvvv <tmdgusv@gmail.com>
2025-10-05 15:06:32 +0900

    🔨 refactor: doctor_main 관련 파일들을 기존의 doctor 디렉토리에 통합 - 기존에는 doctor에 닥터 팝업 관련 파일들, doctor_main 에 닥터 메인 관련 파일이 있었음 - 역할이 헷갈려서 doctor로 통합시켜서 구현하고자 변경함 - routes 도 통합시킴

--- routes/doctor/index.js
+++ routes/doctor/index.js
@@ -1,17 +1,30 @@
-var express = require('express');
+// 메인 페이지 컨트롤러
+const searchMyPatientsPOST = require('../../controllers/doctor_main/searchMyPatientsPOST');
+const cancelRequestPOST = require('../../controllers/doctor_main/cancelRequestPOST');
+const openPatientPageGET = require('../../controllers/doctor_main/openPatientPageGET');
+const getRequestListGET = require('../../controllers/doctor_main/requestListGET')
+const getAllPatientsGET = require('../../controllers/doctor_main/getAllPatientsGET');
+
+// 팝업 페이지 컨트롤러
 const searchPatientPOST = require('../../controllers/doctor/searchPatientPOST');
 const addPatientPOST = require('../../controllers/doctor/addPatientPOST');
 
+var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', async (req, res)=>{
-  res.render('doctor/popupAddPatient');   //(임시)
+  res.render('doctor/doctor_main');
 });
+
+// 새로운 라우트들 추가하기
+router.post('/searchMyPatients', searchMyPatientsPOST);
+router.get('/getRequestList', getRequestListGET);
+router.post('/cancelRequest', cancelRequestPOST);
+router.get('/board/:userId', openPatientPageGET);
+router.get('/getAllPatients', getAllPatientsGET);
 
 // 환자 검색 post 
 router.post('/searchPatient/:searchInput', searchPatientPOST);
-
 // 환자 추가 신청 post
 router.post('/addPatient', addPatientPOST);
 

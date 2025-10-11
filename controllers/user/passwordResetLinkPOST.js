376d74d970c7567c8902da21cde94051ce0e4ddd
vvzvvv <tmdgusv@gmail.com>
2025-10-12 02:17:55 +0900

    🔨 refactor: 비밀번호 찾기 로직에 상수 적용 및 코드 개선 - 응답 상태 코드 및 응답 메시지를 statusCode, responseMessage 상수로 대체함 - 불필요한 console.log()를 제거, require 모듈 순서 정리

--- controllers/user/passwordResetLinkPOST.js
+++ controllers/user/passwordResetLinkPOST.js
@@ -1,17 +1,19 @@
-const smtpTransport = require("../../emailVerificationConfig");
-
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const secretKey = process.env.MY_SECRET;
+const dotenv = require('dotenv');
+dotenv.config();
 
 const db = require('../../models/index');
+const smtpTransport = require("../../emailVerificationConfig");
+const statusCode = require('../../constants/statusCode');
+const responseMessage = require('../../constants/responseMessage');
+
+const secretKey = process.env.MY_SECRET;
 
 const findPassword = async function (req, res) {
 
     const { email } = req.body;
 
     try {
-
         /* 등록된 이메일인지 확인 */
 
         // user 데이터베이스에서 존재하는 환자 이메일인지 확인
@@ -19,13 +21,9 @@ const findPassword = async function (req, res) {
         // doctor 데이터베이스에서 존재하는 의사 이메일인지 확인
         const findDoctor = await db.doctor.findOne({ where: { email: email } });
 
-        console.log(findPatient);
-        console.log(findDoctor);
-
         // 환자, 의사 둘다 찾지 못한 경우
         if (findPatient === null && findDoctor === null) {
-            console.log("등록된 이메일이 아닙니다.");
-            return res.status(404).send({ message: "등록된 이메일이 아닙니다.\n이메일을 확인해주세요." });
+            return res.status(statusCode.NOT_FOUND).send({ message: responseMessage.NOT_FOUND_EMAIL });
         }
 
         let token = '';
@@ -40,8 +38,6 @@ const findPassword = async function (req, res) {
             token = jwt.sign({ email: email, user: "doctor" }, secretKey);
         }
         
-        console.log(token);
-        
         await db.passwordToken.create({ email, token });     // db에 resetToken 모델 생성해야 함
 
         const resetLink = `http://localhost:3000/user/password/change?token=${token}`;      // 서버 url 수정(.env파일 사용)
@@ -55,11 +51,11 @@ const findPassword = async function (req, res) {
         
         await smtpTransport.sendMail(emailOptions);
 
-        res.status(200).send({
-            message: "입력하신 이메일로 비밀번호 초기화 링크를 전송하였습니다.",
+        res.status(statusCode.OK).send({
+            message: responseMessage.SEND_PASSWORD_RESET_LINK_SUCCESS,
         });
     } catch (error) {
-        res.status(500).send({message: "링크 전송 실패이 실패하였습니다.\n나중에 다시 시도해주세요."});
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send({message: responseMessage.SEND_PASSWORD_RESET_LINK_FAIL});
     } finally {
         smtpTransport.close();
     }

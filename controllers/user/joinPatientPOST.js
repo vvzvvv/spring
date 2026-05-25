4afdfa33bd350adb44137485448b2fdb65edfd26
vvzvvv <tmdgusv@gmail.com>
2026-05-25 12:01:50 +0900

    🚓 fix: 사용자 생성 시 응답 상태 코드를 200 에서 201로 수정

--- controllers/user/joinPatientPOST.js
+++ controllers/user/joinPatientPOST.js
@@ -23,7 +23,7 @@ const join = async (req, res) => {
                 password: password
             });
 
-            return res.status(statusCode.OK).send({ message: responseMessage.CREATED_USER });
+            return res.status(statusCode.CREATED).send({ message: responseMessage.CREATED_USER });
         }
     } catch (err) {
         res.status(500).send({
